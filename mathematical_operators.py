10199bcdc6f710af394c380ad9997122f2954fbb
mr-xlr8-AGT <mr.x.l.r.8.pride@gmail.com>
2025-10-19 00:25:33 +0530

    Python tasks.

---
+++ mathematical_operators.py
@@ -0,0 +1,10 @@
+x = int(input("Enter the first number: "))
+y = int(input("Enter the second number: "))
+
+print()
+
+print("Addition:", x + y)
+print("Subtraction:", x - y)
+print("Multiplication:", x * y)
+print("Division:", x / y)
+
