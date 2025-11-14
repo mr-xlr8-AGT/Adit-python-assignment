607126bd93d4539d7a8da35cac00915c5af0df1b
Aditya Gaurav <mr.x.l.r.8.pride@gmail.com>
2025-11-14 20:38:11 +0530

    Task 2 added

---
+++ Assignment 3/math_module_calculations.py
@@ -0,0 +1,10 @@
+# ASSIGNMENT 3 - Task 2: Using the Math Module for Calculations
+import math
+number = float(input("Enter a number: "))
+square_root = math.sqrt(number)
+logarithm = math.log(number)
+sine_value = math.sin(number)
+
+print(f"Square root: {square_root}")
+print(f"Logarithm: {logarithm}")
+print(f"Sine: {sine_value}")
