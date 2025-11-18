66ae0fdb87256b04ab101b52cf1a4e052fd0fdb5
Aditya Gaurav <mr.x.l.r.8.pride@gmail.com>
2025-11-18 23:39:37 +0530

    Task 2 added

---
+++ Assignment 5/List_Slicing.py
@@ -0,0 +1,11 @@
+# List from 1 to 10
+numbers = list(range(1, 11))
+print("Original list:", numbers)
+
+# First five elements extraction
+first_five = numbers[:5]
+print("Extracted first five elements:", first_five)
+
+# Reverse the extracted list
+reversed_five = first_five[::-1]
+print("Reversed extracted elements:", reversed_five)
